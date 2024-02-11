cdb7be09ec32fdab62575edadffd1200d778fac6
kotaranin <158298700+kotaranin@users.noreply.github.com>
2024-02-11 19:49:43 +0100

    Ažuriran dedamraz-kod.c
    Dodate funkcije za inicijalizaciju niti/semafora.

--- dedamraz-kod.c
+++ dedamraz-kod.c
@@ -1,157 +1,166 @@
-#include <stdio.h>
-#include <semaphore.h>
-#include <stdlib.h>
-#include <pthread.h>
 #include <fcntl.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
 #define BROJ_PATULJAKA 9
 #define BROJ_IRVASA 9
 
+pthread_t irvasiNiti[BROJ_IRVASA];
+pthread_t patuljciNiti[BROJ_IRVASA];
+pthread_t dedaMrazNit;
+
+int irvasiNiz[BROJ_IRVASA];
+int patuljciNiz[BROJ_PATULJAKA];
+
 int patuljci;
 int irvasi;
 
-sem_t* dedaMraz;
-sem_t* irvasSemafor;
-sem_t* patuljakSemafor;
-sem_t* mutex;
+sem_t *dedaMraz;
+sem_t *irvasSemafor;
+sem_t *patuljakSemafor;
+sem_t *mutex;
 
 int patuljciPomocna = 0;
 
-void *dedaMrazFunkcija (void *arg) {
-    printf("Deda Mraz nit je pokrenuta!\n");
+void *dedaMrazFunkcija(void *arg) {
+  printf("Deda Mraz nit je pokrenuta!\n");
 
-    while (1) {
+  while (1) {
+    if (patuljciPomocna == BROJ_PATULJAKA) break;
 
-        if(patuljciPomocna == BROJ_PATULJAKA) break;
+    sem_wait(dedaMraz);
+    sem_wait(mutex);
 
-        sem_wait(dedaMraz);
-        sem_wait(mutex);
+    if (irvasi == BROJ_IRVASA) {
+      printf("Deda Mraz priprema sanke\n");
+      for (int r = 0; r < BROJ_IRVASA; r++) sem_post(irvasSemafor);
 
-        if (irvasi == BROJ_IRVASA) {
+      printf("Sanke su pripremljene!\n");
+      irvasi = 0;
 
-            printf("Deda Mraz priprema sanke\n");
-            for (int r=0; r<BROJ_IRVASA; r++) sem_wait(irvasSemafor);
-
-            printf("Sanke su pripremljene!\n");
-            irvasi = 0;
-
-        } else {
-            if(patuljci > 0) printf("Deda Mraz pomaze svim patuljcima koji su trazili pomoc\n");
-            for(int i = 0; i < patuljci; i++) sem_post(patuljakSemafor);
-            patuljci = 0;
-        }
-
-        sem_post(mutex);
+    } else {
+      if (patuljci > 0)
+        printf("Deda Mraz pomaze svim patuljcima koji su trazili pomoc\n");
+      for (int i = 0; i < patuljci; i++) sem_post(patuljakSemafor);
+      patuljci = 0;
     }
 
-    return arg;
+    sem_post(mutex);
+  }
+
+  return arg;
 }
 
-void *irvasiFunkcija (void *arg) {
+void *irvasiFunkcija(void *arg) {
+  int id = *(int *)arg;
+  printf("Irvas broj %d je pokrenut\n", id + 1);
 
-    int id = *(int*) arg;
-    printf("Irvas broj %d je pokrenut\n",id+1);
+  sem_wait(mutex);
 
+  irvasi++;
+  if (irvasi == BROJ_IRVASA) sem_post(dedaMraz);
+
+  sem_post(mutex);
+  sem_wait(irvasSemafor);
+
+  printf("Irvas se prikljucuje Deda Mrazu\n");
+  sleep(2);
+  return arg;
+}
+
+void *patuljakFunkcija(void *arg) {
+  int id = *(int *)arg;
+
+  printf("Patuljak broj %d je pokrenut\n", id + 1);
+
+  int pomoc = rand() % 10;
+
+  if (pomoc > 5) {
     sem_wait(mutex);
-    
-    irvasi++;
-    if (irvasi == BROJ_IRVASA) sem_post(dedaMraz);
+    patuljci++;
+
+    if (patuljci == 3) sem_post(dedaMraz);
+    printf("Patuljak broj %d trazi pomoc od Deda Mraza\n", id + 1);
 
     sem_post(mutex);
-    sem_post(irvasSemafor);
-    printf("Irvas se prikljucuje Deda Mrazu\n");
-    sleep(2);
-    return arg;
 
+    sem_wait(patuljakSemafor);
+    sleep(3);
+  }
+
+  sem_wait(mutex);
+  patuljciPomocna++;
+  if (BROJ_PATULJAKA < patuljciPomocna + 2) sem_post(dedaMraz);
+  sem_post(mutex);
+
+  printf("Patuljak broj %d radi svoj posao\n", id + 1);
+  sleep(2);
+  return arg;
 }
 
-void *patuljakFunkcija (void *arg) {
+void kreiranjeSemafora() {
+  dedaMraz = sem_open("Deda Mraz", O_CREAT, 0644, 0);
+  irvasSemafor = sem_open("Irvasi", O_CREAT, 0644, 0);
+  patuljakSemafor = sem_open("Patuljci", O_CREAT, 0644, 0);
+  mutex = sem_open("Mutex", O_CREAT, 0644, 0);
 
-    int id = *(int*) arg;
-
-    printf("Patuljak broj %d je pokrenut\n",id+1);
-
-    int pomoc = rand()%10;
-
-    if (pomoc>5) {
-        sem_wait(mutex);
-        patuljci++;
-
-        if (patuljci == 3) sem_post(dedaMraz);
-        printf("Patuljak broj %d trazi pomoc od Deda Mraza\n",id+1);
-
-        sem_post(mutex);
-
-        sem_wait(patuljakSemafor);
-        sleep(3);
-
-    }
-
-    sem_wait(mutex);
-    patuljciPomocna++;
-    if (BROJ_PATULJAKA < patuljciPomocna + 2) sem_post(dedaMraz);
-    sem_post(mutex);
-    
-    printf("Patuljak broj %d radi svoj posao\n",id+1);
-    sleep(2);
-    return arg;
-
+  sem_init(dedaMraz, 0, 0);
+  sem_init(irvasSemafor, 0, 0);
+  sem_init(patuljakSemafor, 0, 0);
+  sem_init(mutex, 0, 1);
 }
 
-int main (void) {
+void kreiranjeNiti() {
+  pthread_create(&dedaMrazNit, NULL, dedaMrazFunkcija, NULL);
 
-    patuljci = 0;
-    irvasi = 0;
+  for (int i = 0; i < BROJ_IRVASA; i++) irvasiNiz[i] = i;
 
-    dedaMraz = sem_open("Deda Mraz",O_CREAT,0644,0);
-    irvasSemafor = sem_open("Irvasi",O_CREAT,0644,0);
-    patuljakSemafor = sem_open("Patuljci",O_CREAT,0644,0);
-    mutex = sem_open("Mutex",O_CREAT,0644,0);
+  for (int i = 0; i < BROJ_PATULJAKA; i++) patuljciNiz[i] = i;
 
-    sem_init(dedaMraz, 0, 0);
-    sem_init(irvasSemafor, 0, 0);
-    sem_init(patuljakSemafor,0,0);
-    sem_init(mutex,0,1);
+  for (int i = 0; i < BROJ_IRVASA; i++)
+    pthread_create(&irvasiNiti[i], NULL, irvasiFunkcija, (void *)&irvasiNiz[i]);
+  for (int i = 0; i < BROJ_PATULJAKA; i++)
+    pthread_create(&patuljciNiti[i], NULL, patuljakFunkcija,
+                   (void *)&patuljciNiz[i]);
+}
 
-    pthread_t dedaMrazNit;
-    pthread_create(&dedaMrazNit, NULL, dedaMrazFunkcija, NULL);
+void zatvaranjeSemafora() {
+  sem_close(dedaMraz);
+  sem_unlink("Deda Mraz");
+  sem_destroy(dedaMraz);
 
-    int irvasiNiz[BROJ_IRVASA];
-    for(int i=0;i<BROJ_IRVASA;i++) irvasiNiz[i]=i;
+  sem_close(irvasSemafor);
+  sem_unlink("Irvasi");
+  sem_destroy(irvasSemafor);
 
-    pthread_t irvasiNiti[BROJ_IRVASA];
-    for (int i=0; i<BROJ_IRVASA; i++) pthread_create(&irvasiNiti[i], NULL, irvasiFunkcija, (void*)&irvasiNiz[i]);
-    
-    int patuljciNiz[BROJ_PATULJAKA];
-    for(int i=0;i<BROJ_PATULJAKA;i++) patuljciNiz[i]=i;
-    
-    pthread_t patuljciNiti[BROJ_IRVASA];
-    for (int i=0; i<BROJ_PATULJAKA; i++) pthread_create(&patuljciNiti[i], NULL, patuljakFunkcija, (void*)&patuljciNiz[i]);
+  sem_close(patuljakSemafor);
+  sem_unlink("Patuljci");
+  sem_destroy(patuljakSemafor);
 
-    for(int i = 0; i < BROJ_PATULJAKA; i++) pthread_join(patuljciNiti[i], NULL);
-    for(int i = 0; i < BROJ_IRVASA; i++) pthread_join(irvasiNiti[i], NULL);
-    
+  sem_close(mutex);
+  sem_unlink("Mutex");
+  sem_destroy(mutex);
+}
 
-    pthread_join(dedaMrazNit,NULL);
-    printf("\nKraj!");
+int main(void) {
+  patuljci = 0;
+  irvasi = 0;
 
-    sem_close(dedaMraz);
-    sem_unlink("Deda Mraz");
-    sem_destroy(dedaMraz);
+  kreiranjeSemafora();
 
-    sem_close(irvasSemafor);
-    sem_unlink("Irvasi");
-    sem_destroy(irvasSemafor);
+  kreiranjeNiti();
 
-    sem_close(patuljakSemafor);
-    sem_unlink("Patuljci");
-    sem_destroy(patuljakSemafor);
+  for (int i = 0; i < BROJ_PATULJAKA; i++) pthread_join(patuljciNiti[i], NULL);
+  for (int i = 0; i < BROJ_IRVASA; i++) pthread_join(irvasiNiti[i], NULL);
 
-    sem_close(mutex);
-    sem_unlink("Mutex");
-    sem_destroy(mutex);
+  pthread_join(dedaMrazNit, NULL);
+  printf("\nKraj!\n");
 
-    return 0;
+  zatvaranjeSemafora();
+
+  return 0;
 }
